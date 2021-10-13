d1a9934d9272c9d689cd5e2a804538c36397e234
Emanuele117 <emanuele.pica7@gmail.com>
2021-10-13 20:26:51 +0200

    modifiche css (problema colori scritte buzz ecc.)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,31 @@
 // scrivi un programma che esegua un ciclo da 1 a 100
-let list = document.querySelector('.list')
+let list = document.querySelector('.list');
 
 
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
-    const element = `<li class="box">${i}</li>`
+
+
+    const element = `<li class="box" id="lista">${i}</li>`
+
+
 
 
     if (i % 3 == 0 && i % 5 == 0) {
-        list.innerHTML += `<li class="box">fizzBuzz</li>`
+        list.innerHTML += `<li class="box" id="lista">fizzBuzz</li>`
     } else if (i % 5 == 0) {
-        list.innerHTML += `<li class="box">Buzz</li>`
+        list.innerHTML += `<li class="box" id="lista">Buzz</li>`
     } else if (i % 3 == 0) {
-        list.innerHTML += `<li class="box">Fizz</li>`
+        list.innerHTML += `<li class="box" id="lista">Fizz</li>`
     } else {
-        list.innerHTML += `<li class="box">${i}</li>`
+        list.innerHTML += `<li class="box" id="lista">${i}</li>`
     }
 
 }
 
+document.getElementById("lista").classList += " MyClass";
 
-element.classList + " box_2"
+
+
 //  ogni interazione appenda un elemento html al container con uno stile differente a seconda del valore dell'indice per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5.
 
 /* i % 3 == 0 */
